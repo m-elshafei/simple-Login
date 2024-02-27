5e2d7ecabbd1a1e505a3795e82808cfaf8c57129
Moustafa Elshafei <110654969+m-elshafei@users.noreply.github.com>
2024-02-26 17:44:34 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <App />
+// );
